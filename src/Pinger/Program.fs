e4f4875fa89b26fdf6b7d799486f9439f7857f01
Nicholas J <nicholas.a.johns5@gmail.com>
2019-04-14 17:24:42 -0400

    Add pingHandler

--- src/Pinger/Program.fs
+++ src/Pinger/Program.fs
@@ -4,10 +4,14 @@ open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Http
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open Prometheus
 open Giraffe
 
 let configureApp (app : IApplicationBuilder) =
     // Add Giraffe to the ASP.NET Core pipeline
+    app.UseMetricServer |> ignore
     app.UseGiraffe Handlers.webApp
 
 let configureServices (services : IServiceCollection) =
