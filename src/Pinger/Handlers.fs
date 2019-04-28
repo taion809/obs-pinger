4f0ca4ad05f607c0fd324070ec77c13f5f186c1f
Nicholas J <nicholas.a.johns5@gmail.com>
2019-04-28 15:04:31 -0400

    Fix handlers include prometheus

--- src/Pinger/Handlers.fs
+++ src/Pinger/Handlers.fs
@@ -2,11 +2,16 @@
 
 open Microsoft.AspNetCore.Http
 open FSharp.Control.Tasks.V2.ContextInsensitive
+open Prometheus
 open Giraffe
+
+let pingCounter = Metrics.CreateCounter("pinger_ping_total", "The total number of pings", CounterConfiguration())
 
 let pingHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
         task {
+            pingCounter.Inc() |> ignore
+
             return! text "pong" next ctx
         }
 
